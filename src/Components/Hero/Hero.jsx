729623817f760e10c117487eb5200dd7110c44c4
AllanMuhari <allanmuhari087@gmail.com>
2023-09-03 00:48:23 +0300

    set up

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div></div>
+        <div>
+            <div>
+                <h1></h1>
+                <h1></h1>
+
+                <button></button>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+       <img src='' alt='' className=''></img>
+    </div>
+  )
+}
+
+export default Hero
