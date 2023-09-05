61e83646167e8a3b059f3ae2754a9dba0814cbfd
AllanMuhari <allanmuhari087@gmail.com>
2023-09-05 19:12:03 +0300

    modification

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,10 +5,9 @@ import SocialLink from "../Utils/Social/Social";
 const Hero = ({
   heroapi: { title, subtitle, btntext, img, sociallinks, videos },
 }) => {
-  // console.log(heroapi)
   return (
     <>
-      <div className='lg:relative h-auto w-auto flex flex-col'>
+      <div className='max-w-[1640px] h-auto w-auto flex flex-col'>
         <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10'></div>
         <div className='relative opacity-100 z-20 grid items-center justify-items-center nike-container'>
           <div className='grid items-center justify-items-center mt-28 md:mt-24'>
@@ -24,22 +23,22 @@ const Hero = ({
             >
               {btntext}
             </button>
-            <div className='flex flex-col items-center gap-2 md:gap-3  top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-auto'>
+            <div className='grid items-center gap-5 md:gap-3 absolute top-[33vh] lg:top-[27vh] left-[11%] xl:left-0 w-auto h-auto'>
               {videos?.map((val, i) => (
                 <Clips key={i} imgsrc={val.imgsrc} clip={val.clip} />
               ))}
             </div>
-            <div className='grid items-center  top-[33vh] lg:top-[27vh] right-0 gap-3'>
+            <div className='grid items-center absolute top-[33vh] lg:top-[27vh] right-0 gap-3'>
               {sociallinks?.map((val, i) => (
                 <SocialLink key={i} icon={val.icon} />
               ))}
             </div>
           </div>
-          <div className='flex sm:items-center absolute'>
+          <div className='flex items-center'>
             <img
               src={img}
               alt='hero-img/img'
-              className='w-auto h-[20vh] lg:h-[35vh] md:h-[31vh]  xsm:h-[19vh] transitions-theme -rotate-[30deg] hover:rotate-0 cursor-pointer object-fill'
+              className='w-auto h-[21vh] lg:h-[35vh] md:h-[31vh]  xsm:h-[19vh] transitions-theme -rotate-[30deg] hover:rotate-0 cursor-pointer object-fill'
             />
           </div>
         </div>
