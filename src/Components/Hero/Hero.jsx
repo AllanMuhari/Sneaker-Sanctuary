7b370a9c1d0726acd427261b2127e69652f60d65
AllanMuhari <allanmuhari087@gmail.com>
2023-09-06 00:57:09 +0300

    socials

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,15 +6,14 @@ const Hero = ({
   heroapi: { title, subtitle, btntext, img, sociallinks, videos },
 }) => {
   return (
-    <>
       <div className='h-auto w-auto flex flex-col'>
         <div className='bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auto absolute top-0 left-0 right-0 opacity-100 z-10'></div>
         <div className='relative opacity-100 z-20 grid items-center justify-items-center nike-container'>
           <div className='grid items-center justify-items-center mt-28 md:mt-24'>
-            <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>
+            <h1 className='text-3xl lg:text-6xl md:text-4xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>
               {title}
             </h1>
-            <h1 className='text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>
+            <h1 className='text-3xl lg:text-xl md:text-4xl  xsm:text-2xl font-extrabold filter drop-shadow-sm text-slate-200'>
               {subtitle}
             </h1>
             <button
@@ -43,7 +42,6 @@ const Hero = ({
           </div>
         </div>
       </div>
-    </>
   );
 };
 
--- src/Components/Utils/Social/Social.jsx
+++ src/Components/Utils/Social/Social.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+/** @format */
 
-const Social = () => {
+import React from "react";
+
+const Social = ({ icon }) => {
   return (
-    <div></div>
-  )
-}
+    <>
+      <img
+        src={icon}
+        alt='icon/social'
+        className='w-8 h-8 lg:h-12 lg:w-12 flex items-center cursor-pointer md:w-6 md:h-6  transition-all duration-200 hover:scale-110'
+      />
+    </>
+  );
+};
 
-export default Social
+export default Social;
