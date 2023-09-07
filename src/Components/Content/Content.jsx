b590ab8b6cd2bff3642bef4d8e56bb5b46b553d3
AllanMuhari <allanmuhari087@gmail.com>
2023-09-08 01:52:02 +0300

    styling

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -9,10 +9,10 @@ const Content = ({
   return (
     <div className='flex flex-col items-center justify-between lg:justify-center lg:flex-row nike-container'>
       <div className='grid items-center w-full max-w-lg lg:max-w-none md:text-center lg:justify-items-center'>
-        <h1 className='text-4xl font-bold sm:text-3xl text-gradient'>
+        <h1 className='text-3xl font-bold lg:text-4xl text-gradient'>
           {heading}
         </h1>
-        <h1 className='text-5xl font-bold lg:text-4xl md:text-3xl sm:text-2xl text-slate-900 filter drop-shadow-lg'>
+        <h1 className='text-2xl font-bold lg:text-5xl md:text-4xl text-slate-900 filter drop-shadow-lg'>
           {title}
         </h1>
         <p className='my-4 xl:text-sm text-slate-900'>{text}</p>
@@ -37,7 +37,7 @@ const Content = ({
           className={`w-auto object-fill transitions-theme ${
             ifExists
               ? "h-44 lg:h-56 md:h-52 xsm:h-36 rotate-6 hover:-rotate-12"
-              : "h-48 lg:h-64 md:h-60  xsm:h-40 rotate-[19deg] hover:rotate-12"
+              : "h-48 lg:h-64 md:h-60  xsm:h-40 rotate-[20deg] hover:rotate-12"
           }`}
         />
       </div>
